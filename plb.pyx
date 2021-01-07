307d92247992b53a112c7d8f0f34f1cfb6ebda4b
no <no@no.com>
2021-01-07 16:02:16 +0000

    misc changes

--- plb.pyx
+++ plb.pyx
@@ -6,48 +6,51 @@ cimport numpy as np
 from cpython cimport array
 cimport cython
 
-cdef class PyBook:
-    cdef Book *book
+@cython.boundscheck(False)
+@cython.wraparound(False)
+def l2_walk(self, long[:] _ts, long[:] _side, double[:] _price, double[:] _qty):
+    cdef Book *book = new Book()
+   
+    cdef Py_ssize_t alloc_len = len(_ts)*5*2*2
+    cdef Py_ssize_t T = len(_ts)
+    cdef Py_ssize_t y = 5*2*2
 
-    def __cinit__(self):
-        self.book = new Book()
+    _out_tops = np.full((T,y), np.nan, dtype=np.dtype('d'))
+    cdef double[:,::1] out_tops_view = _out_tops
 
-    def add_bid(self, double price, double qty):
-        self.book.add_bid(price, qty)
+    _out_ts = np.full((T,), np.nan, dtype=np.dtype('long'))
+    cdef long[:] out_ts_view = _out_ts
 
-    def add_ask(self, double price, double qty):
-        self.book.add_ask(price, qty)
+    cdef int i, out_ix
+    cdef double ret[5*2*2]
+    cdef double prev_ret[5*2*2]
 
-    def get_top10(self):
-        cdef double ret[10*2*2]
-        ret[:] = self.book.get_tops(10)
-        return ret
-
-
-cdef class L2Walk:
-    def __cinit__(self, int top_n):
-        pass
-
-    @cython.boundscheck(False)
-    @cython.wraparound(False)
-    def l2_walk(self, long[:] _ts, long[:] _side, double[:] _price, double[:] _qty):
-        cdef int T = len(_ts)
-        cdef Book *book = new Book()
-        
-        _out = np.full(T*10*2*2, 0, dtype=np.dtype('d')).reshape((T, 10*2*2))
-        cdef double[:,::1] out_view = _out
-
-        cdef int i
-        cdef double ret[40]
-
-        for i in range(len(_ts)):
-            if _side[i] == 1:
-                book.add_bid(_price[i], _qty[i])
-            else:
-                book.add_ask(_price[i], _qty[i])
-		
-            ret[:] = book.get_tops(10)
-            out_view[i] = ret
+    out_ix = 0
+    for i in range(len(_ts)):
+        if _side[i] == 1:
+            book.add_bid(_price[i], _qty[i])
+        else:
+            book.add_ask(_price[i], _qty[i])
             
-        return _out
-		    
+        ret[:] = book.get_tops(5)
+
+        # Only append if we dont already have this row
+        if i == 0:
+            out_tops_view[out_ix,:] = ret 
+            out_ts_view[out_ix] = _ts[i]
+
+            prev_ret = ret
+            out_ix = out_ix + 1
+        else:
+            for x in range(y):
+                if ret[x] != prev_ret[x]:
+                    out_tops_view[out_ix,:] = ret
+                    out_ts_view[out_ix] = _ts[i]
+
+                    out_ix = out_ix + 1
+                    break
+        prev_ret = ret
+
+    del book
+    return np.hstack((_out_ts.reshape((T,1)), _out_tops))
+                
